36b536b8e040febde484a46dfd13ae98804d29b4
Chimee <Chimee.eze@gmail.com>
2018-09-03 10:17:06 -0500

    API Test

--- LeWeatherTest_Incomplete.py
+++ LeWeatherTest_Incomplete.py
@@ -26,11 +26,7 @@ def test_one(): #Open main input page
         testlog.write(str(datetime.now()) + ': Forecast Input Page Test Failed Page Title Assertion' + '\n') 
         testlog.close()
         driver.quit()
-    #assert "Property Search" in driver.title
-    #elem = driver.find_element_by_id("fldInput")
-    #elem.send_keys(name)
-    #elem = driver.find_element_by_id("login_pwd")
-    #elem.send_keys(Keys.RETURN)
+
 
 
 def test_two(): #Test API Response after proper input
@@ -43,6 +39,7 @@ def test_two(): #Test API Response after proper input
     try:
         driver.get(base_url)
         assert "Chi's Forecast Finder" in driver.title
+        #Find Input Fields and enter test parameters
         elem = driver.find_element_by_name("city")
         elem.send_keys(city)
         elem = driver.find_element_by_name("state")
@@ -50,13 +47,20 @@ def test_two(): #Test API Response after proper input
         elem = driver.find_element_by_name("zip")
         elem.send_keys(zipcode)
         elem.send_keys(Keys.RETURN)
-        testlog.write(str(datetime.now()) + ': Test_Two Passed' + '\n') 
+        time.sleep(5)
+        #get response body element by name of pre
+        elem=driver.find_element_by_tag_name("pre")
+        datatest = elem.text
+        print(datatest)
+        assert("Austin" in datatest)
+        testlog.write(str(datetime.now()) + ': Test_Two (Proper Input) Passed' + '\n') 
 
         testlog.close()
+        driver.quit()
         #time.sleep(5)
         #driver.quit()
     except Exception:
-        testlog.write(str(datetime.now()) + ': Test_Two Failed' + '\n') 
+        testlog.write(str(datetime.now()) + ': Test_Two (Proper Input) Failed' + '\n') 
         testlog.close()
         driver.quit()
     
@@ -70,6 +74,7 @@ def test_three(): #Test API Response after invalid input
     try:
         driver.get(base_url)
         assert "Chi's Forecast Finder" in driver.title
+        #Find Input Fields and enter test parameters
         elem = driver.find_element_by_name("city")
         elem.send_keys(city)
         elem = driver.find_element_by_name("state")
@@ -77,11 +82,17 @@ def test_three(): #Test API Response after invalid input
         elem = driver.find_element_by_name("zip")
         elem.send_keys(zipcode)
         elem.send_keys(Keys.RETURN)
-        testlog.write(str(datetime.now()) + ': Test_Three Passed' + '\n') 
+        time.sleep(5)
+        #get response body element by name of pre
+        elem=driver.find_element_by_tag_name("pre")
+        datatest = elem.text
+        print(datatest)
+        #check for invalid characters message in response body
+        assert("Your input contains invalid characters." in datatest)        
+        testlog.write(str(datetime.now()) + ': Test_Three (Invalid Characters in input message) Passed' + '\n') 
 
         testlog.close()
-        #time.sleep(5)
-        #driver.quit()
+        driver.quit()
     except Exception:
         testlog.write(str(datetime.now()) + ': Test_Three Failed' + '\n') 
         testlog.close()
@@ -105,40 +116,26 @@ def test_four(): #Test API Response For Proper Routing To OpenWeather API
         elem = driver.find_element_by_name("zip")
         elem.send_keys(zipcode)
         elem.send_keys(Keys.RETURN)
-        testlog.write(str(datetime.now()) + ': Test_Four Passed' + '\n')
+        time.sleep(5)
+        #get response body element by name of pre
+        elem=driver.find_element_by_tag_name("pre")
+        datatest = elem.text
+        print(datatest)
+        assert("Birmingham" in datatest)        
+        testlog.write(str(datetime.now()) + ': Test_Four (Proper Routing To OpenWeather API) Passed' + '\n')
         
         testlog.close()
         #time.sleep(5)
-        #driver.quit()
+        driver.quit()
     except Exception:
         testlog.write(str(datetime.now()) + ': Test_Four Failed' + '\n')
         testlog.close()
         driver.quit()
 
-def test_five(): #Test API Response For Proper Routing To APIXU API
-
-    testlog = open('/Weather API Conflict/logs/testresults.txt', "a")
-    city = "Austin"
-    state = "Texas"
-    zipcode = "78708"
-    driver = webdriver.Chrome(executable_path='C:/Weather API Conflict/chromedriver_win32/chromedriver.exe')
-    try:
-        driver.get(base_url)
-        assert "Chi's Forecast Finder" in driver.title
-        elem = driver.find_element_by_name("city")
-        elem.send_keys(city)
-        elem = driver.find_element_by_name("state")
-        elem.send_keys(state)
-        elem = driver.find_element_by_name("zip")
-        elem.send_keys(zipcode)
-        elem.send_keys(Keys.RETURN)
-        testlog.write(str(datetime.now()) + ': Test_Five Passed' + '\n')
-        testlog.close()
-        #time.sleep(5)
-        #driver.quit()
-    except Exception:
-        testlog.write(str(datetime.now()) + ': Test_Five Failed' + '\n')
-        testlog.close()
-        driver.quit()         
+#Testing API Response For Proper Routing To APIXU API satisfied in Test_two
 
 
+test_one()
+test_two()
+test_three()
+test_four()        
